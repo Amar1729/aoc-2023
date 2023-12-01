ebb914ab460a7d78dae4c146f828fedc76489199
Amar1729 <amar.paul16@gmail.com>
2023-12-01 00:49:48 -0500

    d01p02

--- python/d01/main.py
+++ python/d01/main.py
@@ -22,16 +22,68 @@ def part1(data) -> int:
     return sum(digits)
 
 
+# good god
+def parse_number(s: str, forward: bool) -> int | None:
+    if forward:
+        while s:
+            if (s[0] == "1") or s.startswith("one"):
+                return 1
+            if (s[0] == "2") | s.startswith("two"):
+                return 2
+            if (s[0] == "3") | s.startswith("three"):
+                return 3
+            if (s[0] == "4") | s.startswith("four"):
+                return 4
+            if (s[0] == "5") | s.startswith("five"):
+                return 5
+            if (s[0] == "6") | s.startswith("six"):
+                return 6
+            if (s[0] == "7") | s.startswith("seven"):
+                return 7
+            if (s[0] == "8") | s.startswith("eight"):
+                return 8
+            if (s[0] == "9") | s.startswith("nine"):
+                return 9
+
+            s = s[1:]
+
+    else:
+        while s:
+            if (s[-1] == "1") or s.endswith("one"):
+                return 1
+            if (s[-1] == "2") | s.endswith("two"):
+                return 2
+            if (s[-1] == "3") | s.endswith("three"):
+                return 3
+            if (s[-1] == "4") | s.endswith("four"):
+                return 4
+            if (s[-1] == "5") | s.endswith("five"):
+                return 5
+            if (s[-1] == "6") | s.endswith("six"):
+                return 6
+            if (s[-1] == "7") | s.endswith("seven"):
+                return 7
+            if (s[-1] == "8") | s.endswith("eight"):
+                return 8
+            if (s[-1] == "9") | s.endswith("nine"):
+                return 9
+
+            s = s[:-1]
+
+    return None
+
+
 def part2(data) -> int:
-    total = 0
+    digits = [
+        int(f"{parse_number(line, True)}{parse_number(line, False)}")
+        for line in data
+    ]
 
-    # todo
-
-    return total
+    return sum(digits)
 
 
 if __name__ == "__main__":
     data = parse(sys.argv[1])
 
-    print(part1(data))
+    # print(part1(data))
     print(part2(data))
