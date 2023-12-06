6d78c1bcb8499b11ebbe53d2c97ea008a925e8a9
Amar1729 <amar.paul16@gmail.com>
2023-12-06 00:39:07 -0500

    d06p2

--- python/d06/main.py
+++ python/d06/main.py
@@ -2,16 +2,23 @@
 
 from __future__ import annotations
 
+import math
 import sys
 from pathlib import Path
 from pprint import pprint
 
+# import numpy as np
 
 
-def parse(fname: str) -> list[str]:
+def parse(fname: str, part2=False):
     """Read from data file. Returns problem specific formatted data."""
     with Path(fname).open() as f:
         lines = [line.strip() for line in f.read().splitlines() if line.strip()]
+
+    if part2:
+        time = lines[0].split(":")[1].replace(" ", "")
+        distance = lines[1].split(":")[1].replace(" ", "")
+        return int(time), int(distance)
 
     time = [int(x) for x in lines[0].split(":")[1].split(" ") if x.strip()]
     distance = [int(x) for x in lines[1].split(":")[1].split(" ") if x.strip()]
@@ -37,16 +44,16 @@ def part1(data: list[tuple[int, int]]) -> int:
     return math.prod(sum(good_distance(t, d)) for t, d in data)
 
 
-def part2(data: list[str]) -> int:
-    total = 0
-
-    # todo
-
-    return total
+def part2(data: tuple[int, int]) -> int:
+    time, distance = data
+    # couldn't get this to work nicely(?), but actually the naive solution isn't as slow as i feared.
+    # ~ 5.5 sec
+    # return np.roots([-1, time, -1 * distance])
+    return sum(good_distance(time, distance))
 
 
 if __name__ == "__main__":
     data = parse(sys.argv[1])
 
-    print(part1(data))
-    # print(part2(data))
+    # print(part1(data))
+    print(part2(parse(sys.argv[1], True)))
