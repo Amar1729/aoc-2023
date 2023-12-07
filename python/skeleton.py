1de305b336f6a8624c95326930b8a09543f40316
Amar1729 <amar.paul16@gmail.com>
2023-12-07 01:04:33 -0500

    skeleton: fix up some potential lint violations

--- python/skeleton.py
+++ python/skeleton.py
@@ -10,7 +10,7 @@ import re
 import sys
 import typing
 from pathlib import Path
-from pprint import pprint
+from pprint import pprint  # noqa: F401
 
 # see parent directory
 # from aoc_tools import *
@@ -22,24 +22,18 @@ def parse(fname: str) -> list[str]:
         return [line.strip() for line in f.read().splitlines() if line.strip()]
 
 
-def part1(data: list[str]) -> int:
-    total = 0
-
-    # todo
-
-    return total
+def part1(data) -> int:
+    print(data)
+    return 0
 
 
-def part2(data: list[str]) -> int:
-    total = 0
-
-    # todo
-
-    return total
+def part2(data) -> int:
+    print(data)
+    return 0
 
 
 if __name__ == "__main__":
     data = parse(sys.argv[1])
 
     print(part1(data))
-    print(part2(data))
+    # print(part2(data))
