0084fd20c64f77785e448b7aa5fd83e0572e10f4
Amar1729 <amar.paul16@gmail.com>
2023-12-08 00:46:15 -0500

    d08p2

--- python/d08/main.py
+++ python/d08/main.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 # commonly-used built-in imports. not all of these are necessarily used each day.
+import math
 import sys
 from pathlib import Path
 from pprint import pprint  # noqa: F401
@@ -26,16 +27,25 @@ def parse_node(s: str) -> tuple[str, tuple[str, str]]:
     return (src, (dl, dr))
 
 
-def part1(data) -> int:
+def part1(data, curr: str | None = None) -> int:
     mapp = {}
     for n in data[1]:
         src, dst = parse_node(n)
         mapp[src] = dst
 
-    curr = "AAA"
+    # curr = "AAA"
+    # start_node is optionally specified (by p2)
+    if not curr:
+        # p1 default
+        curr = "AAA"
+        cond = lambda c: c == "ZZZ"  # noqa: E731
+    else:
+        # for p2
+        cond = lambda c: c.endswith("Z")  # noqa: E731
+
     dirs = data[0]
     step = 0
-    while curr != "ZZZ":
+    while not cond(curr):
         dir = dirs[step % len(dirs)]
         # print(dir, curr, mapp[curr], step)
         curr = mapp[curr][0] if dir == "L" else mapp[curr][1]
@@ -45,12 +55,20 @@ def part1(data) -> int:
 
 
 def part2(data) -> int:
-    print(data)
-    return 0
+    curr: set[str] = set()
+    for n in data[1]:
+        src, _ = parse_node(n)
+
+        if src.endswith("A"):
+            curr.update([src])
+
+    # kind of gross forcing part1 to re-form the map each time but whatever
+    steps = [part1(data, curr=p) for p in curr]
+    return math.lcm(*steps)
 
 
 if __name__ == "__main__":
     data = parse(sys.argv[1])
 
-    print(part1(data))
-    # print(part2(data))
+    # print(part1(data))
+    print(part2(data))
