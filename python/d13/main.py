e6618f923b63b27ad5bdb9e92ef2c243039ee154
Amar1729 <amar.paul16@gmail.com>
2023-12-13 01:05:30 -0500

    d13p1

---
+++ python/d13/main.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+
+from __future__ import annotations
+
+import sys
+from pathlib import Path
+from pprint import pprint  # noqa: F401
+
+
+def parse(fname: str) -> list[str]:
+    """Read from data file. Returns problem specific formatted data."""
+    with Path(fname).open() as f:
+        content = f.read()
+
+    return [[line for line in chunk.split()] for chunk in content.split("\n\n")]
+
+
+def fold(s: list[str]) -> int:
+    for col in range(1, len(s[0])):
+        if all(l == r for line in s for l, r in zip(line[:col][::-1], line[col:])):
+            return col
+
+    for row in range(1, len(s)):
+        up = s[:row][::-1]
+        down = s[row:]
+
+        if all(u_row == d_row for u_row, d_row in zip(up, down)):
+            return row * 100
+
+    # print our stuff if we reach err state
+    for l in s:
+        print(l)
+    raise ValueError
+
+
+def part1(data) -> int:
+    return sum(fold(chunk) for chunk in data)
+
+
+def part2(data) -> int:
+    print(data)
+    return 0
+
+
+if __name__ == "__main__":
+    data = parse(sys.argv[1])
+
+    print(part1(data))
+    # print(part2(data))
