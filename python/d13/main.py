f88ad9fe11701e12145764161a3f905f7830c6cf
Amar1729 <amar.paul16@gmail.com>
2023-12-13 01:05:35 -0500

    d13p2

--- python/d13/main.py
+++ python/d13/main.py
@@ -33,17 +33,39 @@ def fold(s: list[str]) -> int:
     raise ValueError
 
 
+def fuzzy_fold(s: list[str]) -> int:
+    for col in range(1, len(s[0])):
+        b = sum(l == r for line in s for l, r in zip(line[:col][::-1], line[col:]))
+        line = s[0]
+        correct = min(len(line[col:]), len(line[:col])) * len(s)
+        if b == correct - 1:
+            return col
+
+    for row in range(1, len(s)):
+        up = s[:row][::-1]
+        down = s[row:]
+
+        b = sum(u_c == d_c for u_row, d_row in zip(up, down) for u_c, d_c in zip(u_row, d_row))
+        correct = min(len(up), len(down)) * len(up[0])
+        if b == correct - 1:
+            return row * 100
+
+    # print our stuff if we reach err state
+    for l in s:
+        print(l)
+    raise ValueError
+
+
 def part1(data) -> int:
     return sum(fold(chunk) for chunk in data)
 
 
 def part2(data) -> int:
-    print(data)
-    return 0
+    return sum(fuzzy_fold(chunk) for chunk in data)
 
 
 if __name__ == "__main__":
     data = parse(sys.argv[1])
 
-    print(part1(data))
-    # print(part2(data))
+    # print(part1(data))
+    print(part2(data))
