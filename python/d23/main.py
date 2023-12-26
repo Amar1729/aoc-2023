0361c31e535574b5e14635b1914e2f39cdf57b10
Amar1729 <amar.paul16@gmail.com>
2023-12-26 18:10:43 -0500

    d23p2

--- python/d23/main.py
+++ python/d23/main.py
@@ -8,16 +8,28 @@ import typing
 from pathlib import Path
 from pprint import pprint  # noqa: F401
 
+import networkx as nx
 import numpy as np
 import numpy.typing as npt
 
 
-def parse(fname: str) -> npt.NDArray[np.unicode_]:
+def parse(fname: str, p2: bool = False) -> npt.NDArray[np.unicode_]:
     """Read from data file. Returns problem specific formatted data."""
     with Path(fname).open() as f:
         lines = [line.strip() for line in f.read().splitlines() if line.strip()]
 
-    return np.array([[c for c in line] for line in lines])
+    arr = np.array([[c for c in line] for line in lines])
+
+    if p2:
+        # correct the input array
+        # is there a nice numpy way to do this?
+        hy, hx = arr.shape
+        for y in range(hy):
+            for x in range(hx):
+                if arr[(y, x)] in "<>v^":
+                    arr[(y, x)] = "."
+
+    return arr
 
 
 def manhattan(p1: tuple[int, int], p2: tuple[int, int]) -> int:
@@ -131,13 +143,74 @@ def part1(data) -> int:
     return len(visited[goal])
 
 
+def neighbors(p: tuple[int, int], g: nx.DiGraph) -> typing.Generator[tuple[int, int], None, None]:
+    for y, x in [
+        (-1, 0),
+        (0, -1),
+        (1, 0),
+        (0, 1),
+    ]:
+        ny, nx = (p[0] + y, p[1] + x)
+        if (ny, nx) in g.nodes:
+            yield (ny, nx)
+
+
+def dfs(
+    start: tuple[int, int], g: nx.DiGraph, path: list[tuple[int, int]],
+) -> typing.Generator[tuple[int, tuple[int, int]], None, None]:
+    adj = dict(g.adjacency())
+    for n in neighbors(start, g):
+        if n != start and n not in path and len(adj[n]) != 2:
+            yield len(path), n
+        elif n != start and n not in path:
+            yield from dfs(n, g, [*path, n])
+
+
+def condense(g: nx.DiGraph) -> nx.DiGraph:
+    adj = dict(g.adjacency())
+
+    edges = []
+
+    for u, _ in filter(lambda e: len(e[1]) != 2, adj.items()):
+        for path_len, v in dfs(u, g, []):
+            if v != u:
+                edges.append((u, v, {"weight": path_len}))
+
+    newg = nx.DiGraph()
+    newg.add_edges_from(edges)
+    return newg
+
+
 def part2(data) -> int:
-    print(data)
-    return 0
+
+    g = nx.grid_2d_graph(*data.shape, create_using=nx.DiGraph)
+    g.remove_edges_from([
+        (a, b)
+        for a, b in g.edges
+        if "#" in (data[a], data[b])
+    ])
+    g.remove_nodes_from([
+        n
+        for n in g.nodes
+        if data[n] == "#"
+    ])
+
+    # takes a while to condense the graph, because i'm doing it in a fairly naive way.
+    g = condense(g)
+
+    start = next(filter(lambda n: n[0] == 0, g.nodes))
+    goal = max(g.nodes, key=lambda n: n[0])
+
+    # naively iterating through all the simple paths that exist in the graph. takes a min or two.
+    return max(
+        sum(1 + g.edges[(u, v)]["weight"] for u, v in zip(path, path[1:]))
+        for path in nx.all_simple_paths(g, start, goal)
+    )
 
 
 if __name__ == "__main__":
     data = parse(sys.argv[1])
+    # print(part1(data))
 
-    print(part1(data))
-    # print(part2(data))
+    data = parse(sys.argv[1], True)
+    print(part2(data))
